c1a17e8964beb5bb1b6660e81f19da6e27f165d1
Mark Cruz <markchriscruz@yahoo.com>
2018-03-05 07:23:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,12 +126,12 @@ class TicTacToe
     end
   end
   
-  def winner(array)
-    if won?(array)
+  def winner
+    if won?
       WIN_COMBINATIONS.each do |win_combination|
-        position_1 = array[win_combination[0]]
-        position_2 = array[win_combination[1]]
-        position_3 = array[win_combination[2]]
+        position_1 = @board[win_combination[0]]
+        position_2 = @board[win_combination[1]]
+        position_3 = @board[win_combination[2]]
         if (position_1 == "X" && position_2 == "X" && position_3 == "X")
           return "X"
         elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
