f1489ea3f7c43084bcfeef44745bc6ea579ec752
Mark Cruz <markchriscruz@yahoo.com>
2018-03-05 07:25:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -145,13 +145,13 @@ class TicTacToe
     end
   end
   
-  def play(board)
-    while !over?(board)
-      turn(board)
+  def play
+    while !over?
+      turn
     end
-    if won?(board)
-      puts "Congratulations " + winner(board) + "!"
-    elsif draw?(board)
+    if won?
+      puts "Congratulations " + winner + "!"
+    elsif draw?
       puts "Cat's Game!"
     else
       #do nothing
